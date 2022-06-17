c415bed9f4d4e240d33d4edd202de40722d68d96
Nastya3147 <93736808+Nastya3147@users.noreply.github.com>
2022-06-17 21:20:21 +0200

    +++

--- src/routers/tasks.js
+++ src/routers/tasks.js
@@ -54,15 +54,9 @@ router.get("/", auth, async (req, res) => {
 });
 router.get("/find", async (req, res) => {
 
-    try {
-        const tasks = await Tasks.find();
-        // if (!tasks.length) {
-        //     res.status(404).send();
-        // }
-        res.status(200).send({tasks: tasks});
-    } catch {
-        res.status(500).send();
-    }
+
+        res.send("<h1>HHHHH</h1>");
+
 });
 // router.get("/:id", auth, async (req, res) => {
 //

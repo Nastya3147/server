59818b2f3ce76709fa02af3116f893c87353c529
Nastya3147 <93736808+Nastya3147@users.noreply.github.com>
2022-06-17 22:30:10 +0200

    Ss

--- src/index.js
+++ src/index.js
@@ -18,13 +18,18 @@ require('./db/mongoose');
  // const userRouter = require("./routers/user");
  // const taskRouter = require("./routers/tasks");
  app.use(express.json());
- app.use('/uploads', express.static('/Diplom/uploads'));
+ // app.use('/uploads', express.static('/Diplom/uploads'));
  // app.use('/user', userRouter);
  app.use(require("./routers/tasks"));
- const PORT=process.env.PORT || 5000
- app.listen(PORT,  ()=> {
-  console.log(`Example app listening on port ${PORT}!`)
- })
+ // const PORT=process.env.PORT || 5000
+ // app.listen(PORT,  err=> {
+ //  if(err) throw err;
+ //  console.log(`Example app listening on port ${PORT}!`)
+ // })
+ const server = app.listen(process.env.PORT || 5000, () => {
+  const port = server.address().port;
+  console.log(`Express is working on port ${port}`);
+ });
  // app.listen(8081, function () {
  //  console.log('Example app listening on port 3000!')
  // })
